422b5aab910bf986cf2b898d574145a34d1c493b
Jontes-Tech <84264740+Jontes-Tech@users.noreply.github.com>
2021-05-16 09:40:07 +0200

    JUU version 1.0

---
+++ JUU.sh
@@ -0,0 +1,100 @@
+#Jontes Update Utitlity 1.0.
+
+#     __            __                __  __          __      __            __ __ __  __  ____
+#    | | ___  _ __ | |_ ___ ___      | | | |_ __   __| | __ _| |_ ___      | | | | |_(_| (_| |_ _   _
+# _  | |/ _ \| '_ \| __/ _ / __|_____| | | | '_ \ / _` |/ _` | __/ _ \_____| | | | __| | | | __| | | |
+#| |_| | (_) | | | | ||  __\__ |_____| |_| | |_) | (_| | (_| | ||  __|_____| |_| | |_| | | | |_| |_| |
+# \___/ \___/|_| |_|\__\___|___/      \___/| .__/ \__,_|\__,_|\__\___|      \___/ \__|_|_|_|\__|\__, |
+#                                          |_|                                                  |___/
+
+#If You are reading this, I know this is a VERY basic script but also my first.
+#I am currently (Open a calculator and do the current year - 2009) years of age and I do tech stuff.
+#This script is for my convenience and I will add more things than just updating.
+
+
+#Include the colors needed.
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+#Main Script
+clear #Starting with a clean slate
+echo -e "${BLUE}Welcome to Jontes updating utility! ${NC}" #Intro Text, To welcome you guys!
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+echo -e "You are using version ${Blue} 1.0 ${NC}" #Showing you the version
+
+sleep ".5" #Giving you some time!
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+echo -e "${RED}If you have paid for this, you have been SCAMMED. It is free to download at my github. ${NC}" #Yup, Isn't it lovely to get scammed? Well, It's free the way you got it. On my github.
+echo -e "${RED}If you didn't get this from my github. STOP THIS SCRIPT! It prob has malware in it! ${NC}" #Yup, Isn't it lovely to get malware? Well, It's free the way you got it. On my github.
+echo -e "${RED}Press CTRL + C to stop this if it any of these apply. ${NC}" #Yup, Isn't it lovely to get scammed? Well, It's free the way you got it. On my github.
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+echo "Starting in 10 seconds!"
+sleep "1"
+echo "Starting in 9 seconds!"
+sleep "1"
+echo "Starting in 8 seconds!"
+sleep "1"
+echo "Starting in 7 seconds!"
+sleep "1"
+echo "Starting in 6 seconds!"
+sleep "1"
+echo "Starting in 5 seconds!"
+sleep "1"
+echo "Starting in 4 seconds!"
+sleep "1"
+echo "Starting in 3 seconds!"
+sleep "1"
+echo "Starting in 2 seconds!"
+sleep "1"
+echo "Starting in 1 seconds!"
+sleep "1"
+
+clear
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+echo -e "${BLUE}Checking lists ${NC} " #Update text
+sleep "1"
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+sudo apt update #Update lists
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+echo -e "${BLUE}Upgrading! ${NC} " #Upgrade text
+sleep "1"
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+sudo apt upgrade #Upgrade
+clear
+
+echo -e "${BLUE}Completed The Updating Process ${NC} " #Completed Text
+
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+echo "*" #Lets seperate stuff!
+
+#Copyright (c) 2021 Jonatan Love Holmgren All Rights Reserved. Using commands from the Linux command line.
